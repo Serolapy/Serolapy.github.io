36defaaed7547191dd5be57ebf7e94e4a3e5aed5
Serolapy <65182656+Serolapy@users.noreply.github.com>
2020-10-03 14:54:58 +0300

    Add files via upload

---
+++ animator/js/js.js
@@ -0,0 +1,36 @@
+/*Выбор типажа клетки*/
+$('#tdType').change(function(){
+	let v = $(this).val(),
+		cat = $('#editCat'),
+		object = $('#editObjects');
+	switch(v){
+		case "empty":
+			cat.css('display','none');
+			object.css('display','none');
+			break;
+		case "cat":
+			cat.css('display','block');
+			object.css('display','none');
+			break;
+		case "object";
+			cat.css('display','none');
+			object.css('display','block');
+			break;
+		case "objectCat":
+			cat.css('display','block');
+			object.css('display','block');
+			break;
+	}
+});
+$('#editSky').change(function(){
+	let s = $(this).val();
+	$('#editSkyPreview').attr('src','https://catwar.su/cw3/sky/'+s+'.png');
+});
+$('#editTime').change(function(){
+	let s = $(this).val();
+	$('#editTimePreview').attr('src','https://catwar.su/cw3/symbole/hours/'+s+'.png');
+});
+$('#editSeason').change(function(){
+	let s = $(this).val();
+	$('#editSeasonPreview').attr('src','https://catwar.su/cw3/symbole/season'+s+'.png');
+});
