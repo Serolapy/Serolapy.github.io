65cc7976ab8e6c300ee9e3b2600ef1d0b35e9f2e
Владислав Иванов <vladi6008@gmail.com>
2020-10-10 15:16:01 +0300

    -

--- animator/js.js
+++ animator/js.js
@@ -34,8 +34,8 @@ $('#editSeason').change(function(){
 	let s = $(this).val();
 	$('#editSeasonPreview').attr('src','https://catwar.su/cw3/symbole/season'+s+'.png');
 });
-function Familie() {
-	$('#Mamapole').html($('#Mutter').val())
-	$('#Papapole').html($('#Vater').val())
-	$('#Bratosisterypole').html($('#MuttGeschwisterer').val())
+function FamilieGeschichteUndAndere() {
+	$('#Mamapole').html($('#Mutter').val());
+	$('#Papapole').html($('#Vater').val());
+	$('#Bratosisterypole').html($('#MuttGeschwisterer').val());
 }
