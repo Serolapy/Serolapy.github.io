e594d662e3f582643d02c8336e6e86e691e7167a
Владислав Иванов <vladi6008@gmail.com>
2020-10-26 16:58:05 +0300

    Добавил изменение данных о коте в превью. Продебажить!!

--- animator/js.js
+++ animator/js.js
@@ -1,3 +1,5 @@
+import { cssNumber } from "jquery";
+
 /*Выбор типажа клетки*/
 $('#tdType').change(function(){
 	let v = $(this).val(),
@@ -58,4 +60,17 @@ $('#Mutter, #Vater, #MuttGeschwisterer, #Cathistory, #catLocation').change(Famil
 $('#newMessBtn').on('click', function () {
 	let text = $('newMessText').val().replace($('#newMessName').val(), '<span class="myname">' + $('#newMessName').val() + '</span>');
 	$('#chat_msg').prepend('<span><hr><table width="100%"><tbody><tr><td style="width: 90%"><span class="chat_text vlm' + $('#editVolume').val() + '"><span>' + text + '</span> - <b class="nick">' + $('#newMessCat').val() + '</b></span></td><td style="width: 10%"><a href="#">➝</a> | <a href="#" class="msg_report">X</a></td></tr></tbody></table></span>');
+})
+
+/*edit cats DATA!!!*/
+function EdCdata(pol, name, job, smell, online) {
+	var cName = '<u><a href="#">' + name + '</a></u><br>',
+		cJob = '<small><i>' + job + '</i></small><br>',
+		cSmell = (pol == 'der') ? 'Его' : 'Её' + 'запах:<br><img src="' + smell + '"><br>',
+		cOnline = '[' + (online == 'online') ? '<font color="#006400">В игре</font>' : (online == 'offline') ? '<font color="#A52A2A">Спит</font>' : (online == 'wait') ? '<font color="#333333">Недавно ушёл</font>' : (online == 'delete') ? '<font color="#333366">На удалении</font>' : '<font color="#333366">Заблокирован</font>' + ']';
+	return '<span class="cat_tooltip">' + cName + cJob + cSmell + cOnline + '</span>'
+}
+$('#previewcatBtn').on('click', function () {
+	/*По нажатию на кнопку*/
+	$('#previewDataCat').html(EdCdata($('catPOL').val(), $('#catName').val(), $('#catJob').val(), $('#catSmell').val(), $('#catOnline').val()));
 })
