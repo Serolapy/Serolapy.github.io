c9c8ccaf42ff0b2a4bcaf945bb154197450ebed9
Serolapy <65182656+Serolapy@users.noreply.github.com>
2020-12-25 16:36:59 +0300

    Update gamePole.js

--- animator/js/gamePole.js
+++ animator/js/gamePole.js
@@ -2,7 +2,30 @@ function start(){
 	$('a, input').click(function(e){
 		e.preventDefault();
 	});
-	alert('Загружено');
+	
+	$(document).mousedown(function(e){
+		if (e.which == 3 && confirm('Сделать скриншот?')){
+			//гугл - это как смысл жизни
+			
+			//плавное перемещение вверх
+			$('html, body').stop().animate({
+				scrollTop: $('#app').offset().top
+			}, 777);
+			
+			//получение даты и времени
+			
+			var date = new Date(),
+				resDate = 'CW_Anim_St/' + date.getDate() + '/' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+				
+			//скриншот
+			html2canvas($('#app')[0]).then(canvas=>{
+				var link = document.createElement("a");
+				link.href = canvas.toDataURL('image/png');
+				link.download = resDate + '.png';
+				link.click();
+			})
+		}
+	});
 }
 
 if(window.jQuery){
@@ -11,3 +34,4 @@ if(window.jQuery){
 	console.log('Нет jQuery');
 	setTimeout(start, 5000);
 }
+
