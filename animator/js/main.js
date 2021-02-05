d7918d3f1ff05b28ff5ee8cc41e72d816a1acb9d
Serolapy <65182656+Serolapy@users.noreply.github.com>
2021-02-05 19:06:52 +0300

    Обновление (добавлены переходы и прочее)

--- animator/js/main.js
+++ animator/js/main.js
@@ -11,9 +11,91 @@ $('#app a, #app input').click(function(e){
 	e.preventDefault();
 });
 
+
 //установка норм размера шапки
 const height = 1600;									//высота изображения дефолт
 const width = 300;										//ширина изображения дефолт
 
 var windowWidth = parseFloat($("header").css('width')); //ширина хидера
 $("header, header > table").css('height', windowWidth/height*width);	//магия
+
+
+//Функция для таблиц с выборочным колвом строк
+//на таблицу класс tbl_plus
+//на + класс tbl_plus_add
+//на - класс tbl_plus_remove
+$('.tbl_plus_add').on('click', function(){
+	var plus_btn = $(this);
+	var table = $(this).parent().parent().parent().parent();
+	var tbody = $(table + ' > tbody');
+	//$('.tbl_plus_add').parent().parent().parent().parent().children().eq('0').html()
+	
+	var tr = '<tr><td><input type="text" class="' + (plus_btn.data('class')? plus_btn.data('class') : '') + '"></td><td><input type="button" value="-" class="tbl_plus_remove"></td></tr>';
+	tbody.append(tr);
+	
+	$('.tbl_plus_remove').on('click', function(){
+		var minus_btn = $(this);
+		minus_btn.parent().parent().remove();
+	});
+});
+
+//меню выбора действий в #app .cage
+$('.cage').on('contextmenu', function(e){
+	e.preventDefault();
+	$('.select_cage').removeClass('select_cage');
+	$(this).addClass('select_cage');
+	
+	/*https://coderoad.ru/4909167/*/
+	function mouseX(evt) {
+		if (evt.pageX) {
+			return evt.pageX;
+		} else if (evt.clientX) {
+		return evt.clientX + (document.documentElement.scrollLeft ?
+			document.documentElement.scrollLeft :
+			document.body.scrollLeft);
+		} else {
+			return null;
+		}
+	}
+	
+	function mouseY(evt) {
+		if (evt.pageY) {
+			return evt.pageY;
+		} else if (evt.clientY) {
+			return evt.clientY + (document.documentElement.scrollTop ?
+			document.documentElement.scrollTop :
+			document.body.scrollTop);
+		} else {
+			return null;
+		}
+	}
+	/*end*/
+	
+	$('#cage_menu').css('top', mouseY(e)).css('left',mouseX(e)).css('display','block');
+	$('body').on('click', function(){
+		$('#cage_menu').css('display', 'none');
+		$('.select_cage').removeClass('select_cage');
+		$('body').off('click');
+	});
+});
+//действия в #cage_menu
+$('#cage_menu > a').on('click', function(e){
+	e.preventDefault();
+	if ($('.select_cage').length == 0){
+		$('#cage_menu').css('display', 'none');
+		console.error('Клетка в #app не выбрана, но кнопка нажата');
+		return
+	}
+	var funct = $(this).data('function'),
+		cage = $('.select_cage');
+	
+	switch(funct){
+		case 'empty':
+			cage.html('');
+			$('#cage_menu').css('display', 'none');
+			$('.select_cage').removeClass('select_cage');
+			break;
+		case 'items':
+			break;		
+	}	
+});
--- animator/js/newcat.js
+++ animator/js/newcat.js
@@ -1,30 +1,178 @@
+/*отлов старых ошибок*/
+
+//коты
+try{
+	if(!JSON.parse(localStorage.getItem('oldCats'))){
+		localStorage.setItem('oldCats', JSON.stringify([]));
+	}
+}
+catch(e){
+	if (e instanceof SyntaxError){
+		localStorage.setItem('oldCats', JSON.stringify([]));
+	}
+}
+//переходы
+try{
+	if(!JSON.parse(localStorage.getItem('moves'))){
+		localStorage.setItem('moves', JSON.stringify([]));
+	}
+}
+catch(e){
+	if (e instanceof SyntaxError){
+		localStorage.setItem('moves', JSON.stringify([]));
+	}
+}
+
+
 /*
-	создание кота
+	конструктор котов
 */
-function addCat(pol, name, job, smell, online, color, costume, size, items) {
+function New_cat(pol='der', name='Системолап', job='Воитель', smell='https://catwar.su/cw3/odoroj/51.png', online='block', color='https://catwar.su/cw3/composited/d2bb67dfe46197cf.png', costume='', size='100', mouth=''){
+	/*
+			Входные данные (совместно с функцией codeCat())
+			
+		>pol - пол персонажа; 
+		Тип: string;
+		Возможные значения: 
+			'der' - муж.,
+			'die' - жен.;
+		По умолчанию: 'der';
+		Заметки:
+		
+		>name - имя персонажа;
+		Тип: string;
+		Возможные значения: любая строка;
+		По умолчанию: 'Системолап';
+		Заметки:
+		
+		>job - должность персонажа;
+		Тип: string;
+		Возможные значения: любая строка;
+		По умолчанию: 'Воитель';
+		Заметки:
+		
+		>job - запах племени персонажа;
+		Тип: string;
+		Возможные значения: прямая ссылка на картинку;
+		По умолчанию: 'https://catwar.su/cw3/odoroj/51.png';
+		Заметки:
+		
+		>online - состояние игрока (онлайн?);
+		Тип: string;
+		Возможные значения: 
+			'online' - "В сети", 
+			'offline' - "Спит",
+			'wait' - "Недавно ушёл/ушла",
+			'delete' - "На удалении",
+			'block' - "Заблокирован(-а)";
+		По умолчанию: 'block';
+		Заметки:
+		
+		>color - окрас персонажа;
+		Тип: string;
+		Возможные значения: прямая ссылка на картинку;
+		По умолчанию: 'https://catwar.su/cw3/composited/d2bb67dfe46197cf.png' (окрас "Енот");
+		Заметки:
+		
+		>costume - костюмы и дефекты персонажа;
+		(!!!)Тип: string;
+		Возможные значения: прямые ссылки на картинки;
+		По умолчанию: '';
+		Заметки:
+			~перечиление должно идти через пробел, например:
+				'[окрас1] [дефект1] [дефект2]'
+				'[окрас1]'
+		
+		>size - размер персонажа; 
+		Тип: integer;
+		Возможные значения: числа от 1 до 100;
+		По умолчанию: 100;
+		Заметки:
+			~если допущена ошибка, и значение не вписывается в рамки, то программа вернёт значение 100
+			~если допущена ошибка типа, то программа преобразует строку в целое число
+		
+		>mouth - предметы во рту персонажа;
+		(!!!)Тип: string;
+		Возможные значения: прямые ссылки на картинки;
+		По умолчанию: '';
+		Заметки:
+			~перечиление должно идти через пробел, например:
+				'[предмет1] [предмет2] [предмет2]'
+				'[предмет1]'
+		
+	*/
+	this.pol = pol;
+	this.name = (name==''?'Системолап':name);
+	this.job = (job==''?'Воитель':job);
+	this.smell = (smell==''?'https://catwar.su/cw3/odoroj/51.png':smell);
+	this.online = online;
+	this.color = (color==''?'https://catwar.su/cw3/composited/d2bb67dfe46197cf.png':color);
+	this.costume = costume.split(' ');
+	if(Number(size) <= 0 || Number(size) > 100){
+		this.size = 100;
+	}else{
+		this.size = Number(size);
+	}
+	
+	this.mouth = mouth.split(' ');
+	
+	//сохранение в localStorage
+	this.save = function(){
+		if(confirm('Сохранить персонажа?')){
+			var cats = JSON.parse(localStorage.getItem('oldCats'));
+			cats.push(this);
+			localStorage.setItem('oldCats', JSON.stringify(cats));
+			updateDataHome();
+		}
+	}
+	
+	//превью
+	this.preview = function(){
+		$('#previewDataCat').html('<div class="cage_items">' + codeCat(this) + '</div>');
+	}
+}
 
+//код кота по его данным
+function codeCat(katze){
+	//katze - текущий персонаж
+	
 	//
-	// кот
+	//кот
 	//
-
 	var cat = '';
 	/*костюмы и дефекты*/
-	for (i = 0; i < costume.length; i++) {
-		cat += '<div style="background-image:url(\''+ costume[i] + '\');background-size:' + size + '%; position: absolute;"></div>';
+	for (i = 0; i < katze.costume.length; i++) {
+		cat += '<div style="background-image:url(\''+ katze.costume[i] + '\');background-size:' + katze.size + '%; position: absolute;"></div>';
 	}
 	/*окрас*/
-	cat = '<div style="background-image:url(\'' + color + '\');background-size:' + size + '%;" class="d">' + cat + '</div>';
-
+	cat = '<div style="background-image:url(\'' + katze.color + '\');background-size:' + katze.size + '%;" class="d">' + cat + '</div>';
+	/*какой-то странный div*/
+	cat = '<div>'+cat+'</div>';
+	
+	
+	
 	//
 	//сведения
 	//
-
-	var dataCat = '',
-		cName = '<u><a href="#">' + name + '</a></u><br>',
-		cJob = '<small><i>' + job + '</i></small><br>',
-		cSmell = ((pol == 'der') ? 'Его ' : 'Её ') + 'запах:<br><img src="' + smell + '"><br>',
-		cOnline = '[';
-	switch (online) {
+	
+	var cName = '<u><a href="#">' + katze.name + '</a></u><br>',
+		cJob = '<small><i>' + katze.job + '</i></small><br>',
+		cSmell = ((katze.pol == 'der') ? 'Его ' : 'Её ') + 'запах:<br><img src="' + katze.smell + '"><br>',
+		cOnline = '[ ',
+		cMouth  = '';
+		
+	/*рот*/
+	if(katze.mouth.length != 0 && katze.mouth[0]!=''){
+		cMouth = 'У ' + ((katze.pol == 'der') ? 'него ' : 'неё ') + 'во рту:';
+		cMouth += '<ol class="mouth">';
+		for(i=0;i<katze.mouth.length;i++){
+			cMouth += '<li><img src="'+katze.mouth[i]+'"></li>';
+		}
+		cMouth += '</ol>'
+	}
+	
+	/*статус*/
+	switch (katze.online) {
 		case 'online':
 			cOnline += '<font color="#006400">В игре</font>';
 			break;
@@ -32,149 +180,210 @@ function addCat(pol, name, job, smell, online, color, costume, size, items) {
 			cOnline += '<font color="#A52A2A">Спит</font>';
 			break;
 		case 'wait':
-			cOnline += '<font color="#333333">Недавно ' + ((pol == 'der') ? 'ушёл' : 'ушла') + '</font>';
+			cOnline += '<font color="#333333">Недавно ' + ((katze.pol == 'der') ? 'ушёл' : 'ушла') + '</font>';
 			break;
 		case 'delete':
 			cOnline += '<font color="#333366">На удалении</font>';
 			break;
 		case 'block':
-			cOnline += '<font color="#333366">Заблокирован' + ((pol == 'der') ? '' : 'а') + '</font>';
+			cOnline += '<font color="#333366">Заблокирован' + ((katze.pol == 'der') ? '' : 'а') + '</font>';
 			break;
 	}
-	cOnline += ']';
-	dataCat = '<span class="cat_tooltip" style="z-index: 9999;padding: 5px; min-width: 80px; background: RGBA(255, 255, 255, 0.9); border: 1px solid gray; border-radius: 6px; color: #930; font-weight: bold; text-align: center;" >' + cName + cJob + cSmell + cOnline + '</span>';
-
+	cOnline += ' ]';
+	
+	/*сборка сведений*/
+	
+	//стили нужны?
+	var dataCat = '<span class="cat_tooltip">' + cName + cJob + cMouth + cSmell + cOnline + '</span>';
+		
+	
+	
 	//
-	//сборка кота и данных
+	//стрела
 	//
-
-	var CatAndData = '<span class="catWithArrow"><span class="cat">' + cat + dataCat + '</span></span>';
-
+	
+	var arrow = '';
+	if(false){
+		//добавка наполнения
+	}
+	
+	
+	
 	//
-	//клетка
-	return CatAndData
+	//сборка
+	//
+	var CatAndData = '<span class="cat">' + cat + dataCat + '</span>',
+		CatWithArrow = '<span class="catWithArrow">' + arrow + CatAndData + '</span>';
+	
+	return CatWithArrow
 }
 
-/*сохранение кота*/
-$('#saveCat').on('click', function () {
-	//
-	//local Storage
-	//
-	var Ls = localStorage.getItem('oldCats');
-	if(Ls == null){
-		Ls = '';
-	}
-	// 
-	newcat = '<div class="cage_items">' + addCat(
-		$('#catPOL').val(),
-		$('#catName').val(),
-		$('#catJob').val(),
-		$('#catSmell').val(),
-		$('#catOnline').val(),
-		$('#catColor').val(),
-		$('#catCostume').val().split(','),
-		Number($('#catSize').val())
-	) + '</div>';
-	if (confirm('Сохранить персонажа?')) {
-		Ls += newcat;
-	}
-	localStorage.setItem('oldCats', Ls);
-});
-//кнопка превью
+//предпросмотр
 $('#previewcatBtn').on('click', function () {
-	/*По нажатию на кнопку*/
-	$('#previewDataCat').html('<div class="cage_items">' + addCat(
+	new New_cat(
 		$('#catPOL').val(),
 		$('#catName').val(),
 		$('#catJob').val(),
 		$('#catSmell').val(),
 		$('#catOnline').val(),
 		$('#catColor').val(),
-		$('#catCostume').val().split(','),
-		Number($('#catSize').val())
-	) + '</div>');
+		$('#catCostume').val(),
+		$('#catSize').val(),
+		$('#catMouth').val(),
+	).preview();
 });
+
+//сохранение
+$('#saveCat').on('click', function () {
+	new New_cat(
+		$('#catPOL').val(),
+		$('#catName').val(),
+		$('#catJob').val(),
+		$('#catSmell').val(),
+		$('#catOnline').val(),
+		$('#catColor').val(),
+		$('#catCostume').val(),
+		$('#catSize').val(),
+		$('#catMouth').val(),
+	).save();
+});
+
+/*
+	конструктор переходов
+*/
+function New_moves(img='', name=''){
+	/*
+			Входные данные (совместно с функцией codeCat())
+			
+		>img - картинка перехода; 
+		Тип: string;
+		Возможные значения: прямая ссылка на картинку;
+		По умолчанию: '';
+		Заметки:
+		
+		>name - текст под картинкой;
+		Тип: string;
+		Возможные значения: любая строка;
+		По умолчанию: '';
+		Заметки: 
+			название локации, куда переходит кот;
+	*/
+	this.img = img;
+	this.name = name;
+	
+	this.save = function(){
+		if(confirm('Сохранить переход?')){
+			var moves = JSON.parse(localStorage.getItem('moves'));
+			moves.push(this);
+			localStorage.setItem('moves', JSON.stringify(moves));
+			updateDataHome();
+		}
+	}
+	this.preview = function(){
+		$('#previewMoves').html('<div class="cage_items">' + codeMoves(this) + '</div>');
+	}
+}
+//код перехода
+function codeMoves(moves){
+	return '<span class="move_parent"><img src="'+ moves.img +'" class="move_img"> <span class="move_name">'+ moves.name + '</span></span>'
+	
+}
+//превью перехода
+$('#previewmovesBtn').on('click', function () {
+	new New_moves(
+		$('#movesBackground').val(),
+		$('#movesName').val()
+	).preview();
+});
+//сохранение перехода
+$('#saveMoves').on('click', function () {
+	new New_moves(
+		$('#movesBackground').val(),
+		$('#movesName').val()
+	).save();
+});
+
+
+
 //обновление в >HOME<
-$('#oldCatBtn').on('click', function () {
-	$('#oldCats').html(localStorage.getItem('oldCats'));
+function updateDataHome(){
+	/*персонаж*/
+	var cats = JSON.parse(localStorage.getItem('oldCats')),
+	divCat = '';
+	for(catNum=0;catNum<cats.length;catNum++){
+		divCat += '<div class="cage_items" data-type="cat" data-id="'+catNum+'">' + codeCat(cats[catNum]) + '</div>';
+	}
+	$('#oldCats').html(divCat);
+	
+	/*переход*/
+	var moves = JSON.parse(localStorage.getItem('moves')),
+	divMoves = '';
+	for(movesNum=0;movesNum<moves.length;movesNum++){
+		divMoves += '<div class="cage_items" data-type="moves" data-id="'+movesNum+'">' + codeMoves(moves[movesNum]) + '</div>';
+	}
+	$('#oldMoves').html(divMoves);
+	
 	
 	/*выборка кота*/
-	$('#oldCats > .cage_items').on('click', function(){
+	$('.editData .cage_items').on('click', function(){
+		if($(this).hasClass('select')){
+			$(this).removeClass('select');
+			return
+		}
 		$('.select').removeClass('select');
 		$(this).addClass('select');	
 	});
-});
+};
+updateDataHome();
 
-//добавка предметов под котом
-$('#BtnItemListCAGE').on('click', function(){
-	var items = $('#itemListCAGE').val().split(',');
-	//
-	//предметы в клетке
-	//
-
-	var allItems = ''; //все предметы
-	for (i = 0; i < items.length && i < 5; i++) {
-		let a = '';
-		switch (i) {
-			case 0:
-				a = '0% 0%';
+//удаление предметов
+$('#delete_select').on('click', function(){
+	if(confirm('Удалить выбранный предмет?')){
+		//если предмет выбран
+		if ($('.select').length == 0){
+			return
+		}
+		//тип предмета
+		switch($('.select').data('type')){
+			case 'cat':
+				var i = $('.select').data('id'),
+					cats = JSON.parse(localStorage.getItem('oldCats'));
+				cats.splice(i,1);
+				localStorage.setItem('oldCats', JSON.stringify(cats));
+				updateDataHome();
 				break;
-			case 1:
-				a = '100% 0%';
-				break;
-			case 2:
-				a = '0% 100%';
-				break;
-			case 3:
-				a = '100% 100%';
+			case 'moves':
+				var i = $('.select').data('id'),
+					moves = JSON.parse(localStorage.getItem('moves'));
+				moves.splice(i,1);
+				localStorage.setItem('moves', JSON.stringify(moves));
+				updateDataHome();
 				break;
 		}
-		allItems += 'url(\'' + items[i] + '\') ' + a + ' no-repeat' + ((i == items.length - 1) ? '' : ',');
-	}
-	
-	//великий Хлебушек во тьме, помоги мне...
-	
-	//добавление предметов под котов
-	if ($('.select').length > 0){
-		$('.select').css('background', allItems);
-	} else if($('.select').length == 0){
-		alert('Нужно выбрать изменяемого кота');
 	}
 });
 
-
-/*постановка кота в клетку поля*/
-
-//ЖЕСТКИЙ ДЕБАГ в разделе выборки предметов под котом
+//постановка пердметов в клетку
 $('.cage').on('click', function(){
-	//если кот выбран
+	//если предмет выбран
 	if ($('.select').length == 0){
 		return
 	}
-	var type = $('#tdType').val(),
-	cat = $('.select'),
-	
-	cage = $(this);
-	
-	cat.removeClass('select');
-	
-	switch (type){
-		case 'empty':
-			cage.html('');
-			break;
+	//тип объекта
+	switch($('.select').data('type')){
 		case 'cat':
-			var back = cat.css('backgroundImage');
+			var catID = $('.select').data('id'),
+				cage = $(this),
+				cats = JSON.parse(localStorage.getItem('oldCats'));
 			
-			cat.css('backgroundImage','');
-
-			cage.html('<div class="cage_items">'+cat.html()+'</div>');
-			cat.css('backgroundImage', back);
+			cage.html('<div class="cage_items">' + codeCat(cats[catID]) + '</div>');
 			break;
-		case 'object':
-			cage.html('<div class="cage_items" style=\'background-image:'+cat.css('backgroundImage')+';\'></div>');
-			break;
-		case 'objectCat':
-			cage.html('<div class="cage_items" style=\'background-image:'+cat.css('backgroundImage')+';\'>' + cat.html() + '</div>');
+		case 'moves':
+			var movesID = $('.select').data('id'),
+				cage = $(this),
+				moves = JSON.parse(localStorage.getItem('moves'));
+			
+			cage.html('<div class="cage_items">' + codeMoves(moves[movesID]) + '</div>');
 			break;
 	}
 });
